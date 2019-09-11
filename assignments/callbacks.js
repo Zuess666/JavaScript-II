a1999f726549d0c33e6614433ce4d82497d3fb38
Michael Lowe <mrlowememphis@yahoo.com>
2019-09-10 21:08:19 -0500

    michael-lowe

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -40,93 +40,53 @@ const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
-}
-
-arr = [1, 12, 3, 2, 1, 5, 3, 4, 20]; //Sample array is taken
-
-function print(array) {
-
-array.forEach(function(eachName, index) {
-
-console.log(index + 1 + ". " + eachName);
-
-});
-
+  return cb(arr.length);
 }
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-}
 
- return cb(arr[arr.length - 1]);
-  
-  }
+  return cb(arr[arr.length - 1]);
+}
+last(items, last => {
+  console.log(last);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
 
-  return cb(x + y);
-  
-  
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
-
-return cb(x * y);
-
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+
+  return cb(result);
 }
-
-let result = false;
-
-for (i = 0; i < list.length; i++) {
-
-if (list[i] == item) {
-
-result = true;
-
-break;
-
-}
-
-}
-
-return cb(result);
-
-
 
 /* STRETCH PROBLEM */
 
-function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
-}
+//function removeDuplicates(array, cb) {
+// removeDuplicates removes all duplicate values from the given array.
+// Pass the duplicate free array to the callback function.
+// Do not mutate the original array.
+// }
 
-let result = [];
+// let result = [];
 
-for (i = 0; i < array.length; i++) {
+// for (i = 0; i < array.length; i++) {
+//   let exists = false;
 
-let exists = false;
+//   for (j = 0; j < result.length; j++) {
+//     if (array[i] == result[j]) exists = true;
+//   }
 
-for (j = 0; j < result.length; j++) {
+//   if (!exists) result.push(array[i]);
+// }
 
-if (array[i] == result[j])
-
-exists = true;
-
-}
-
-if (!exists)
-
-result.push(array[i]);
-
-}
-
-return cb(result);
-
-
+// return cb(result);
